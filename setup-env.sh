6ec77be0d39c85a20ee7dc7743ca719d18703e05
Sergei Pronichev <clack8926@gmail.com>
2026-06-01 12:31:23 +0300

    automate log-cleaner and cron setup in setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -22,3 +22,15 @@ apt-get install -y git curl jq docker.io
 echo "Все необходимые инструменты установлены"
 
 echo 'Allias lla="ls -la" установлен. Используй команду: source ~/.bashrc для перезагрузки'
+
+echo "--- Настройка системного очистителя логов ---"
+
+# Копируем скрипт в системную папку.
+sudo cp ./log-cleaner.sh /usr/local/bin/log-cleaner
+sudo chmod +x /usr/local/bin/log-cleaner
+
+# Создаём тестовую папку, если её нет
+mkdir -p /home/clack92/devops-auto/testlogs
+
+# Прописываем задачу в cron для суперпользователя
+(sudo crontab -l 2>/dev/null; echo "0 0 * * * /usr/local/bin/log-cleaner /home/clack92/devops-auto/testlogs 5 >> /var/log/log-cleaner.log 2>&1") | sudo crontab -
